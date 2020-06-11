a8f2da60d205669434faafd26a489f9081167eba
Piotr Jura <>
2020-06-11 14:09:44 +0200

    #005 Understanding asset()/mix() difference, cache bustion with version(), including assets, quickly covering Laravel routes & blade view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,98 +5,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>BlogQL</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </head>
 
 <body>
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">Home</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
+    <div id="app"></div>
+    <div class="md:flex">
+        <div class="md:flex-shrink-0">
+            <img class="rounded-lg md:w-56"
+                src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
+                alt="Woman paying for a purchase">
         </div>
-        @endif
-
-        <div class="content">
-            <div class="title m-b-md">
-                Laravel
-            </div>
-
-            <div class="links">
-                <a href="https://laravel.com/docs">Docs</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://blog.laravel.com">Blog</a>
-                <a href="https://nova.laravel.com">Nova</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://vapor.laravel.com">Vapor</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-            </div>
+        <div class="mt-4 md:mt-0 md:ml-6">
+            <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Marketing</div>
+            <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding
+                customers for your new business</a>
+            <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five
+                ideas
+                you can use to find your first customers.</p>
         </div>
     </div>
 </body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,7 @@ mix.postCss('resources/css/app.css', 'public/css', [
     require('tailwindcss'),
 ])
 mix.js('resources/js/app.js', 'public/js')
+
+if (mix.inProduction()) {
+    mix.version()
+}
