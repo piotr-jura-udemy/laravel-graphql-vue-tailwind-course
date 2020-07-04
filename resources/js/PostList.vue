8b01dd4df1068addb48229afed29a397f39441e3
Piotr Jura <>
2020-07-04 14:23:54 +0200

    #018 apollo option to declare queries, name matching, gql tag, ES2015 template literals & tagged templates, writing first query in component

--- resources/js/PostList.vue
+++ resources/js/PostList.vue
@@ -1,3 +1,20 @@
 <template>
   <div>PostList</div>
 </template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+  apollo: {
+    posts: gql`
+      {
+        posts {
+          id
+          title
+        }
+      }
+    `
+  }
+};
+</script>
