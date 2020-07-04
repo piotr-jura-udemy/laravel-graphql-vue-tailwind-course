042842e510e10eca82a353d87b1d0da32f943e4c
Piotr Jura <>
2020-07-04 18:26:28 +0200

    #020 - Tailwind Layout, padding/margin, centering horizontally, mobile first & responsive design, width, text size

--- resources/js/PostList.vue
+++ resources/js/PostList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    PostList
+  <div class="container mx-auto px-4 w-full md:w-3/4 lg:w-3/5 xl:w-1/2 mb-20 mt-20">
+    <h2 class="text-4xl">All Posts</h2>
     <div v-if="$apollo.loading"></div>
     <div v-else>
       <div v-for="post in posts" :key="post.id">{{ post.id }} is titled: {{ post.title }}</div>
