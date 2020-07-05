bbecf242cfe2da82da9b462b70cb64ee04fa6501
Piotr Jura <>
2020-07-05 19:27:01 +0200

    #021 Component props, router-link, pseudo classes in Tailwind

--- resources/js/PostList.vue
+++ resources/js/PostList.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="container mx-auto px-4 w-full md:w-3/4 lg:w-3/5 xl:w-1/2 mb-20 mt-20">
+  <div class="container mx-auto px-4 w-full md:w-3/4 lg:w-3/5 xl:w-1/2 my-20">
     <h2 class="text-4xl">All Posts</h2>
     <div v-if="$apollo.loading"></div>
+
     <div v-else>
-      <div v-for="post in posts" :key="post.id">{{ post.id }} is titled: {{ post.title }}</div>
+      <PostListItem v-for="post in posts" :key="post.id" :post="post" class="mt-10"></PostListItem>
     </div>
   </div>
 </template>
 
 <script>
 import gql from "graphql-tag";
+import PostListItem from "./components/PostListItem";
 
 export default {
+  components: {
+    PostListItem
+  },
   apollo: {
     posts: gql`
       {
         posts {
           id
           title
+          lead
         }
       }
     `
---
+++ resources/js/components/PostListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link :to="{name: 'post', params: {id: post.id}}">
+      <h4 class="text-2xl pb-3 font-semibold hover:underline">{{ post.title }}</h4>
+    </router-link>
+
+    <p class="text-gray-600 pb-3">{{ post.lead }}</p>
+    <div class="text-sm text-gray-600">By Piotr Jura&nbsp;in Links&nbsp;• 3 hours ago</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: Object
+  }
+};
+</script>
