792898be206c0d2e1f75d5ddb81062567852a132
Piotr Jura <>
2020-07-06 19:48:23 +0200

    #022 Adding avatar column, changing UserFactory to include faces

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,5 +24,10 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'avatar' => collect([
+            'ahmad.jpg', 'akio.jpg', 'britney.jpg', 'daniel.jpg',
+            'harry.jpeg', 'john.png', 'jordan.jpg', 'june.jpg',
+            'rob.png', 'marcel.jpg', 'tim.jpg'
+        ])->random()
     ];
 });
