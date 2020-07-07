ec4f2af497d76c764ad5c2e45d407bce53cb51ac
Piotr Jura <>
2020-07-07 14:45:19 +0200

    #023 Styling Post, Flexbox, flex direction, flex justifying, rounded images

--- resources/js/Post.vue
+++ resources/js/Post.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Post 1</div>
+  <div class="container mx-auto px-4 w-full md:w-3/4 lg:w-3/5 xl:w-1/2 mt-20">
+    <div>
+      <div class="text-lg text-gray-600">By Piotr Jura in Links • 3 hours ago</div>
+
+      <h1 class="text-5xl mt-10 font-bold mb-12">The post title</h1>
+
+      <p class="text-gray-700 pb-3 mb-12 whitespace-pre-line">The blog post content</p>
+
+      <div class="mb-24 flex">
+        <div class="mr-6">
+          <img src="/storage/faces/ahmad.jpg" alt="Author avatar" class="w-16 h-16 rounded-full" />
+        </div>
+        <div class="flex flex-col justify-center">
+          <div class="text-xl text-gray-600">Written by Piotr Jura</div>
+          <div class="text-gray-600">Published in Links on May 19, 2020</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
